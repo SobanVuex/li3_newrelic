0760ff830ccbcfc93754359c7bd13215ac9762ec
Alex Soban <me@soban.co>
2014-09-14 16:38:03 -0400

    - Check if \NewRelic\NewRelic is already loaded - Use 5.4 array syntax

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,17 +1,13 @@
 <?php
 
-use lithium\core\Libraries;
-use li3_newrelic\extensions\NewRelic;
+use \lithium\core\Libraries;
+use \li3_newrelic\extensions\NewRelic;
 
-if (!Libraries::get('NewRelic')) {
-    if (!defined('COMPOSER_VENDOR_PATH')) {
-        define('COMPOSER_VENDOR_PATH', dirname(LITHIUM_APP_PATH) . '/vendor');
-    }
-
-    Libraries::add('NewRelic', array(
+if (class_exists('\\NewRelic\\NewRelic')) {
+    Libraries::add('NewRelic', [
         'bootstrap' => false,
-        'path' => COMPOSER_VENDOR_PATH . '/sobanvuex/php-newrelic/src/NewRelic'
-    ));
+        'path' => dirname(LITHIUM_APP_PATH) . '/vendor/sobanvuex/php-newrelic/src/NewRelic'
+    ]);
 }
 
 NewRelic::init(Libraries::get('li3_newrelic'));
